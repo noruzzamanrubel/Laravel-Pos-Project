ad45fa52f8a7975c90156da6e31c1c1b972f1199
Noruzzaman Rubel <noruzzamanrubel@gmail.com>
2021-04-11 16:34:20 +0600

    project all migration create

--- database/migrations/2021_04_11_100642_create_users_table.php
+++ database/migrations/2021_04_11_100642_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('admin_id')->nullable();
+            $table->foreignId('group_id');
+            $table->string('name', 100);
+            $table->string('email', 100)->nullable();
+            $table->string('phone', 15)->nullable();
+            $table->string('address', 200)->nullable();
             $table->timestamps();
         });
     }
